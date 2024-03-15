0dc3920d5f2ed78ed9ce306345e1cafbb8b7f4c3
unknown <2314290103@ACADEMICO.IESB.BR>
2024-03-15 10:51:34 -0300

    codigos da aula 5

--- aulas/aula05/router.js
+++ aulas/aula05/router.js
@@ -3,7 +3,40 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/produtos", function(req, res){
-    res.json([])
+    res.json([]); // 200 e JSON
+});
+
+router.get("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1) {
+        res.json({}); // 200 e JSON
+    } else {
+        res.status(404).json({msg:"Produto não encontrado"});
+    }
+});
+
+router.post("/produtos", function(req, res){
+    if (req.body && req.body.nome && req.body.preco){
+    res.status(201).json({});
+    } else {
+        res.status(422).json({msg: "Nome e preço do produto obrigatorios"});
+    }
+});
+
+router.put("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1){
+        res.json({});
+    } else {
+        res.status(404).json({msg: "Produto não encontrado"});
+    }
+});
+
+router.delete("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1) {
+        res.status(204).end();
+    } else {
+        res.status(404).json({msg: "Produto não identificado"});
+    }
+   
 });
 
 module.exports = router;
